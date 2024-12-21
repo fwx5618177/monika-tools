485d543cd352783c91232f33647aa273f1a76b6f
fwx5618177 <fengwenxuan2006@126.com>
2024-12-21 12:43:39 +0800

    update: add tg-sale-bot

--- packages/tg-sale-bot/bot/bot.ts
+++ packages/tg-sale-bot/bot/bot.ts
@@ -1,20 +1,20 @@
-import { Telegraf } from "telegraf";
-import { HttpsProxyAgent } from "https-proxy-agent";
+import { Telegraf } from 'telegraf';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 
-const bot = new Telegraf("7441831801:AAFcGe3CWtiAYc9KR57ZDJhpP8BzgIoay0Q", {
+const bot = new Telegraf('xxx', {
   telegram: {
-    agent: new HttpsProxyAgent("http://192.168.3.173:7890"),
+    agent: new HttpsProxyAgent('http://192.168.3.173:7890'),
   },
 });
 
 bot.start((ctx) => {
-  ctx.reply("Welcome! Click the button below to open the Web App.", {
+  ctx.reply('Welcome! Click the button below to open the Web App.', {
     reply_markup: {
       inline_keyboard: [
         [
           {
-            text: "Open Web App",
-            web_app: { url: "https://tg-sale-bot.vercel.app/" },
+            text: 'Open Web App',
+            web_app: { url: 'https://tg-sale-bot.vercel.app/' },
           },
         ],
       ],
@@ -22,14 +22,14 @@ bot.start((ctx) => {
   });
 });
 
-bot.on("message", (ctx) => {
-  ctx.reply("Hello! Click the button below to open the Web App.", {
+bot.on('message', (ctx) => {
+  ctx.reply('Hello! Click the button below to open the Web App.', {
     reply_markup: {
       inline_keyboard: [
         [
           {
-            text: "Open Web App",
-            web_app: { url: "https://tg-sale-bot.vercel.app/" },
+            text: 'Open Web App',
+            web_app: { url: 'https://tg-sale-bot.vercel.app/' },
           },
         ],
       ],
